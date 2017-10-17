b3f174e7e641a1a1d73a8ff4be3bf3f21816d42e
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-17 14:17:04 -0600

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
 
+var CryptoJS = require("crypto-js");
+
+var hash = CryptoJS.SHA256("Message");
+
+console.log(hash);
 /**
  * Adds commas to a number
  * @param {number} number
