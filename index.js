245bbcb108140706910b010393925a410ba0d4ff
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-17 16:13:33 -0600

    quick update

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var SHA256 = require("crypto-js/sha256");
 var CryptoJS = require("crypto-js");
 
 var hash = CryptoJS.SHA256("Message");
+//example url change
+//hash = CryptoJS
 
-console.log(hash.toString(CryptoJS.enc.Base64));
+console.log(hash.toString(CryptoJS.SHA256.Base64));
 /**
  * Adds commas to a number
  * @param {number} number
