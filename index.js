eb2ede7796f89babcba3fb1f23872bf106864e6c
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-18 15:46:03 -0600

    0.1.3

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 'use strict';
-var AES = require("crypto-js/aes");
+//var AES = require("crypto-js/aes");
 var SHA256 = require("crypto-js/sha256");
 
 var CryptoJS = require("crypto-js");
-
+//To run type node index.js
 var hash = CryptoJS.SHA256("Message");
 //example url change
 //hash = CryptoJS
+
+var t = new Date();
+var year = t.getUTCFullYear();
+
+var lastLogin = year - 1;
+
+
+
+// function timeCounter() {
+
+//     var timeDiffer = year - lastLogin;
+
+//     if (timeDiffer > 1) {
+//         timeDiffer = "Greater than 1 year";
+//     }
+
+//     return timeDiffer;
+// }
+// console.log(timeCounter());
 
 console.log(hash.toString(CryptoJS.SHA256.Base64));
 /**
@@ -15,6 +34,22 @@ console.log(hash.toString(CryptoJS.SHA256.Base64));
  * @param {string} locale
  * @return {string}
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
-};
+
+//Be sure to have the location correct!
+ 
+
+// app.put('/user', function (req, res, next) {
+    
+//     mongoose.connect('mongodb://localhost/songs');
+//     user.email; /*= req.body.email;*/
+//     var db = mongoose.connection;``}
+// mongoose.connect('mongodb://localhost/songs');
+// var db = mongoose.connection;
+
+//req.body.email;
+ 
+
+
+// module.exports = function (number, locale) {
+//         return number.toLocaleString(locale);
+//     };
