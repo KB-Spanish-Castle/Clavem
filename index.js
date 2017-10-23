79ce82b5806878ff2b47500982e51c8eb6b3569c
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-23 14:30:13 -0600

    0.3.4

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ function timeCounter() {
     return timeDiffer;
 }
 //
-function initialize(word){
+function initialize(){
 
-   // var word = "";
+    var word = "password";
     var hash = CryptoJS.SHA256(word);
     
 
