71139af2e05bd04900a16c9636bf3c26f43a9f5a
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-11-01 15:25:21 -0600

    1.0.1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require("express");
 var SHA256 = require("crypto-js/sha256");
 var CryptoJS = require("crypto-js");
 var mongoose = require('mongoose');
-var nodeSchedule = require('node-schedule');
+var schedule = require('node-schedule');
 var app = express();
 var User = require('../../models/user.js');
 mongoose.connect('mongodb://localhost/songs');
@@ -17,7 +17,21 @@ var hour = t.getUTCHours();
 const nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
 var grabArray = [];
+//var dateObj = new Date();
+var dateFull = new Date(new Date().getTime()).toLocaleDateString();
+//new Date(new Date().getTime()).toLocaleDateString(); 
+//var dateCompare = user[i].lastUpdate - dateObj;
+var second = 1000 * 1;
+var minute = second * 60;
+var hour = minute * 60;
+var day = hour * 24;
+var week = day * 7;
+var month = day * 30;
+var year = day * 365;
+//Important to set how much time the email notification to update password:
+var passwordAlert = ((year * 0) + (1 * month) + (0 * week) + (0 * day) + (0 * hour) + (0 * minute) + (0 * second));
 
+var howOftenEmailSent = ((year * 0) + (1 * month) + (0 * week));
 function timeCounter() {
   console.log(":TEST SUCESS");
   function findUser() {
@@ -39,21 +53,20 @@ function timeCounter() {
       console.log(grabArray);
       console.log("GrabARRAY+" + grabArray[0]);
       console.log("grabArray is: " + grabArray.length);
-      emailAll(user);
+
+      console.log('UPDATE ACCOUNT PASSWORD CHECKER!');
+      //function myFunction() {
+      setInterval(function () {
+        emailAll(user);
+        console.log("UPDATE WORK 5 SEC");
+      }, howOftenEmailSent);
 
       return grabArray;
 
     });
 
-    console.log("grabArray is: " + grabArray.length);
-
-
 
   }
-
-
-  console.log("grabArray is: " + grabArray.length);
-
 
 
   function emailAll(user) {
@@ -61,16 +74,25 @@ function timeCounter() {
 
     console.log("Time to Grab loop");
     console.log("grabArray is: " + user);
-    //grab aRRAY UNDEF
+
     for (var i = 0; i < grabArray.length; i++) {
 
-      grabEmailData = user[i].email;
-      console.log("User.email is: " + user.email);
-      // var j = schedule.scheduleJob({hour: 14, minute: 30, month: 0}, function(){
-      serverEmail("Machine", process.env.MAIL_NAME, process.env.MAIL_PASSWORD, grabEmailData, "Urgent", "Urgent! Please change your password, it's out of date!", 'mongodb://localhost/songs', User);
+      console.log(user[i].lastUpdate);
+      console.log("DateFull: " + Date.parse(dateFull) + " The month is " + month);
+
+      var calcDates = Date.parse(dateFull) - Date.parse(user[i].lastUpdate);
+
+      if (calcDates > passwordAlert) {
+        console.log("SUCESS SUCESS!!!")
+        grabEmailData = user[i].email;
+
+        console.log("grabEmailData is : " + grabEmailData);
+
+        serverEmail("Machine", process.env.MAIL_NAME, process.env.MAIL_PASSWORD, grabEmailData, "Urgent", "Urgent! Please change your password, it's out of date!", 'mongodb://localhost/songs', User);
+
+      }
 
       console.log('REMINDERS WENT THROUGH');
-      // });
 
     }
 
